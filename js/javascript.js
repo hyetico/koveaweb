4914390c32b044d348db0a3fa268e7420ef4e23c
KHK <apple@appleui-MacBookPro.local>
2022-07-05 00:21:13 +0900

    home

--- js/javascript.js
+++ js/javascript.js
@@ -39,12 +39,38 @@ new Swiper('.header_outer .notice_box .swiper',{
   new Swiper('.commingsoon_right .swiper',{
     direction:'vertical',
     autoplay:true,
-    delay:2000,
+    delay:3000,
     loop:true,
 
     navigation: {
-      prevEl: ".sns_banner_outer .btn_sns_arrows .up_arrow",
-      nextEl: ".sns_banner_outer .btn_sns_arrows .dw_arrow"
+      prevEl: ".commingsoon_right .btn_comming_arrows .up_arrow",
+      nextEl: ".commingsoon_right .btn_comming_arrows .dw_arrow"
+    }
+  })
+
+  new Swiper('.theme02 .swiper',{
+    slidesPerView: 2,
+    direction:'vertical',
+    autoplay:true,
+    delay:3000,
+    loop:true,
+
+    navigation: {
+      prevEl: ".btn_theme_arrows02 .btn_theme_arrow.up_arrow",
+      nextEl: ".btn_theme_arrows02 .btn_theme_arrow.dw_arrow"
     }
   })
   
+
+  new Swiper('.theme01 .swiper',{
+    slidesPerView: 2,
+    direction:'vertical',
+    autoplay:true,
+    delay:3000,
+    loop:true,
+
+    navigation: {
+      prevEl: ".btn_theme_arrows01 .btn_theme_arrow.up_arrow",
+      nextEl: ".btn_theme_arrows01 .btn_theme_arrow.dw_arrow"
+    }
+  })
