77a75bc01d82f3b70b903ef76f4464b2779c88cc
KHK <apple@appleui-MacBookPro.local>
2022-07-06 02:30:21 +0900

    home

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,148 @@
+let hideBtn = document.querySelector('.side_top_btn')
 
-  
+ window.addEventListener('scroll',()=>{
+  let scrollTop = document.documentElement.scrollTop || window.scrollY
+
+      if(scrollTop>500){
+          gsap.to(hideBtn, 0.5, { 
+            opacity: 1
+          })
+          }else {
+            gsap.to(hideBtn, 0.5, { 
+              opacity: 0
+            })
+          }
+      })
+
+
+    // topBtn.addEventListener('click',()=>{
+    //   //gsap.to(요소, 시간, {애니메이션옵션(객체)})
+    // gsap.to(window, 0.5, { 
+    //   scrollTo:0
+    // })
+    // })
+
+      
+
+
+//헤더 검색창
+    const searchVar = document.querySelector('.search_var')
+
+    searchVar.addEventListener('focus',()=>{
+    searchVar.style.width='90%'
+    searchVar.setAttribute('placeholder',' 검색어를 입력하세요.')
+    })
+
+    searchVar.addEventListener('blur',()=>{
+      searchVar.style.width=''
+      searchVar.setAttribute('placeholder','')
+    })
+
+
+    //logo 텍스트 애니메이션
+    const logoText = document.querySelector('.header_center_outer .logo .logo_txt')
+    let text = document.querySelector('.header_center_outer .logo .logo_txt').innerText
+    let split = text.split('').join("</span><span>")
+    split = "<span>"+ split + "</span>"
+    logoText.innerHTML=split
+
+    let spans = document.querySelectorAll('.header_center_outer .logo .logo_txt span')
+    
+      // window.addEventListener('scroll',()=>{
+      //   const headerOuter = document.querySelector('.header_outer')
+      //   let scrollTop = document.documentElement.scrollTop || window.scrollY
+
+      //  if(scrollTop){
+      //    spans.forEach((span, index)=>{
+      //      setInterval(()=>{
+      //        span.classList.add('show')
+      //      },50 * index)
+      //    })
+      //   }
+
+      //   })
+
+          spans.forEach((span, index)=>{
+            setTimeout(()=>{
+              span.classList.add('show')
+            },100 * index)
+          })
+
+
+      
+
+          const menuBtn = document.querySelector('.search_nav')
+          const menu = document.querySelector('.search_outer .menu_sub_search')
+
+          const lnbBtn = document.querySelector('.lnb_nav')
+          const lnbsub = document.querySelector('.lnb_outer .sub_lnb_outer')
+          let isClick = false
+
+          menuBtn.addEventListener('click',()=>{
+            if(isClick==false){
+              gsap.to(menu, 0.2, { 
+                top: '60px',
+                display:'block'
+              },200)
+              isClick = true
+             
+            }else{
+              gsap.to(menu, 0.2, { 
+                top: '-500px',
+              },200)
+              isClick = false
+            }
+          })
+
+          lnbBtn.addEventListener('click',()=>{
+
+            if(isClick==false){
+          
+              gsap.to(lnbsub, 0.2, { 
+                top: '60px',
+              },200)
+              isClick = true
+
+            }else{
+              
+              gsap.to(lnbsub, 0.2, { 
+                top: '-300px',
+              },200)
+              isClick = false
+            }
+          })
+
+
+          const tabList = document.querySelectorAll('.season_menu_tabbtn > li');
+          const contents = document.querySelectorAll('.season_tab_conts .seatab_cont')
+          let activeCont = ''; // 현재 활성화 된 컨텐츠 (기본:#tab1 활성화)
+          
+          for(var i = 0; i < tabList.length; i++){
+            tabList[i].querySelector('.tabbtn').addEventListener('click', function(e){
+              e.preventDefault();
+              for(var j = 0; j < tabList.length; j++){
+                // 나머지 버튼 클래스 제거
+                tabList[j].classList.remove('on');
+          
+                // 나머지 컨텐츠 display:none 처리
+                contents[j].style.display = 'none';
+              }
+          
+              // 버튼 관련 이벤트
+              this.parentNode.classList.add('on');
+          
+              // 버튼 클릭시 컨텐츠 전환
+              activeCont = this.getAttribute('href');
+              document.querySelector(activeCont).style.display = 'block';
+            });
+          }
+
+
+
+
+
+
+  // Swiper
 new Swiper('.header_outer .notice_box .swiper',{
     direction:'vertical',
     autoplay:true,
@@ -14,7 +157,7 @@ new Swiper('.header_outer .notice_box .swiper',{
 
   new Swiper('.visual_main_outer .swiper',{
     autoplay:true,
-    delay:3000,
+    delay:3500,
     loop:true,
 
     navigation: {
